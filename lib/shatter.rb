4bb5c46f3f1d7ca8d16db9a759e37bafb0261a18
Eric Roos <ericroos@hey.com>
2022-12-27 18:59:33 -0600

    init

---
+++ lib/shatter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shatter/version"
+
+module Shatter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shatter/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Shatter
+  VERSION = "0.1.0"
+end
